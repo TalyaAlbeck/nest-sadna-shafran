5a0aeb0d58db3c3c274bfe83beaa518091b31930
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-12-02 15:44:56 +0200

    opem modoules and conrollers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentsModule } from './students/students.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { ChaptersModule } from './chapters/chapters.module';
 
 @Module({
-  imports: [],
+  imports: [StudentsModule, TeacherModule, ChaptersModule],
   controllers: [AppController],
   providers: [AppService],
 })
